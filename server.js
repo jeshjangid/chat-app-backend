4dd4a9b5a8fa3026407c27cdf6babadbf033007a
jeshjangid <jeshjangid09@gmail.com>
2026-06-21 20:26:21 +0530

    origin update

--- server.js
+++ server.js
@@ -14,10 +14,7 @@ const app = express();
 const server = http.createServer(app);
 const allowedOrigins = [
   process.env.CLIENT_URL,
-  'http://localhost:3000',
-  'http://localhost:5173',
-  'http://127.0.0.1:3000',
-  'http://127.0.0.1:5173',
+  '*',
 ].filter(Boolean);
 
 const corsOptions = {
